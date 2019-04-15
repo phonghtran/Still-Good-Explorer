5a240bc1e2e7cae36c3ee1af8ef3c96f847253a4
Phong Tran <phong.tran1@wipro.com>
2019-04-15 17:05:31 -0400

    server tweak

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 // server.js
 // https://medium.com/netscape/deploying-a-vue-js-2-x-app-to-heroku-in-5-steps-tutorial-a69845ace489
 // https://stackoverflow.com/questions/48277747/refresing-a-vue-app-gives-cannot-get-path
+// var express = require('express');
+// var path = require('path');
+// var serveStatic = require('serve-static');
+// app = express();
+// app.use(serveStatic(__dirname + "/dist"));
+// var port = process.env.PORT || 5000;
+// app.listen(port);
+// console.log('server started '+ port);
+
 
 const express = require('express');
 const path = require('path');
@@ -21,7 +30,7 @@ app.get('/', function (req, res) {
   res.render(path.join(__dirname + '/dist/index.html'));
 });
 
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(process.env.PORT || 5000, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
