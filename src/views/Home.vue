305df0dc6093dbd7e41b11d0080e995dda8fc768
phonghtran <phong@phonghtran.com>
2021-11-03 14:16:48 -0400

    new pl

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,41 +1,42 @@
 <template>
   <div>
-
-    <h1 v-bind:style="{'background-color': getColor() }">Still Good Explorer</h1>
+    <h1 v-bind:style="{ 'background-color': getColor() }">
+      Still Good Explorer
+    </h1>
 
     <p>
-      This represents one of the only reasons I'm still using iTunes as my primary music source. I've been keeping a
-      running playlist since forever. And iTunes lets you export that playlist from the app. Which means I have years
-      worth of what I was listening to at any given point. The main data set goes back to 2011. But in a twist of
-      foresight, I digitalized all the mix CDs I made back in high school. Which means I also have insight to 2000 to
-      2004 (the ripe age of 14-18.)
+      This represents one of the only reasons I'm still using iTunes as my
+      primary music source. I've been keeping a running playlist since forever.
+      And iTunes lets you export that playlist from the app. Which means I have
+      years worth of what I was listening to at any given point. The main data
+      set goes back to 2011. But in a twist of foresight, I digitalized all the
+      mix CDs I made back in high school. Which means I also have insight to
+      2000 to 2004 (the ripe age of 14-18.) -Phong
     </p>
 
-    <h2 v-bind:style="{'background-color': getColor() }">
+    <h2 v-bind:style="{ 'background-color': getColor() }">
       <router-link to="/playlists">Playlists &rarr;</router-link>
     </h2>
 
     <p>
-      This is every playlist visualized next to each other. Every song that stayed on from week-to-week is connected by
-      a line.
+      This is every playlist visualized next to each other. Every song that
+      stayed on from week-to-week is connected by a line.
     </p>
 
-    <h2 v-bind:style="{'background-color': getColor() }">
+    <h2 v-bind:style="{ 'background-color': getColor() }">
       <router-link to="/songs">Songs &rarr;</router-link>
     </h2>
 
-    <p>
-      A bit slow, but you can search through every song here.
-    </p>
+    <p>A bit slow, but you can search through every song here.</p>
 
-    <h2 v-bind:style="{'background-color': getColor() }">
+    <h2 v-bind:style="{ 'background-color': getColor() }">
       <router-link to="/genres">Genres &rarr;</router-link>
     </h2>
 
     <p>
-      A visualization of the genre breakdowns. You can tap or hover to see a specific genre.
+      A visualization of the genre breakdowns. You can tap or hover to see a
+      specific genre.
     </p>
-
   </div>
 </template>
 
@@ -45,32 +46,27 @@
   import { colorMixin } from "../mixins/colorMixin";
 
   export default {
-
-    name: 'genres',
+    name: "genres",
     components: {},
-    mixins: [
-      colorMixin
-    ],
+    mixins: [colorMixin],
     methods: {
       getColor: function () {
         const color = this.generateColor();
 
         return color.hex.color;
-      }
-    }
+      },
+    },
   };
 </script>
 
 <style scoped lang="scss">
   @import "@/styles/main.scss";
 
-  h1, h2 {
+  h1,
+  h2 {
     color: $white;
     display: inline-block;
     padding: 0 map_get($spacers, 2);
-
-
-
   }
 
   h2 {
@@ -85,20 +81,15 @@
       color: $white;
       cursor: pointer;
 
-
       &:hover {
         background-color: transparent;
         color: $white;
         text-decoration: none;
-
       }
     }
   }
   p {
     max-width: 28rem;
     margin-bottom: map_get($spacers, 5);
-
   }
-
-
 </style>
